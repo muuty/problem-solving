453a63b1ab7141c2ec7af627ecdfb1659afd69e9
muuty <muuty.me@gmail.com>
2021-07-18 22:48:43 +0900

    solve leetcode 0711

--- Codejam/2021/Round1B/DigitBlocks.py
+++ Codejam/2021/Round1B/DigitBlocks.py
@@ -1,36 +1,57 @@
-import sys
-input = sys.stdin.readline
+
 
 T, N, B, P =list(map(int, input().split()))
 
 
-def fill_bottom(number, blocks, sizes):
-    for i in range(len(sizes)):
-        if (sizes[i]) < N-1:
+def fill_bottom(number, blocks, block_lens):
+    for i in range(len(blocks)):
+        if block_lens[i] < B-2:
             blocks[i].append(number)
+            block_lens[i] += 1
             return i + 1
     return 0
 
 
-def fill_top(number, blocks, sizes):
-    for i in range(len(sizes)):
-        if (sizes[i]) == N-1:
+def fill_top(number, blocks, block_lens):
+    for i in range(len(blocks)):
+        if block_lens[i] == B-1:
             blocks[i].append(number)
+            block_lens[i] += 1
             return i+1
     return 0
 
+def fill_second_top(number, blocks, block_lens):
+    for i in range(len(blocks)):
+        if block_lens[i] == B-2:
+            blocks[i].append(number)
+            block_lens[i] += 1
+            return i+1
+    return 0
+
+
+
 for t in range(T):
     blocks = [[] for j in range(N)]
-    sizes = [0 for j in range(N)]
+    block_lens = [0 for j in range(N)]
     answer = 0
     for i in range(N*B):
         number = int(input())
-        if number < 6:
-            answer = fill_bottom(number, blocks, sizes)
-            if not answer:
-                answer = fill_top(number, blocks, sizes)
-        else:
-            answer = fill_top(number, blocks, sizes)
-            if not answer:
-                answer = fill_bottom(number, blocks, sizes)
+        if number < 7:
+            answer = fill_bottom(number, blocks, block_lens)
+            if answer == 0:
+                answer = fill_second_top(number, blocks, block_lens)
+                if answer == 0:
+                    answer = fill_top(number, blocks, block_lens)
+        elif number == 8 or number == 7:
+            answer = fill_second_top(number, blocks, block_lens)
+            if answer == 0:
+                answer = fill_bottom(number, blocks, block_lens)
+                if answer == 0:
+                    answer = fill_top(number, blocks, block_lens)
+        elif number == 9:
+            answer = fill_top(number, blocks, block_lens)
+            if answer == 0:
+                answer = fill_second_top(number, blocks, block_lens)
+                if answer == 0:
+                    answer = fill_bottom(number, blocks, block_lens)
         print(answer)
--- programmers/BinarySearch/1.py
+++ programmers/BinarySearch/1.py
@@ -1,26 +1,23 @@
+import heapq
+
+
 def solution(n, times):
-    answer = 0
+    speeds = [1.0 / time for time in times]
 
-    leng = len(times)
-    left = 1
-    right = (leng+1) * max(times) # 최대 범위
+    total_speed = sum(speeds)
+    required_time = int(n / total_speed)
+    passed = [int(required_time * speed) for speed in speeds]
+    print(passed)
+    lines = []
+    for i in len(times):
+        heapq.heappush(lines, [(passed + 1) * times[i], times[i]])
 
-    while left <= right:
-        mid = (left + right) // 2
+    for i in range(n - sum(passed)):
+        line = heapq.heappop(lines)
+        if i == n - 1:
+            return line[0]
+        line[0] += line[1]
+        heapq.heappush(lines, line)
 
-        count = 0
-        for time in times:
-            count += mid // time
-            # 심사 인원수를 넘으면 다음 단계
-            if count >= n: break
 
-        # n명을 심사 할 수 있는 경우
-        # 한 심사관에게 주어진 시간을 줄여본다.
-        if count >= n:
-            answer = mid
-            right = mid - 1
-        # 없는 경우
-        elif count < n:
-            left = mid + 1
-
-    return answer
+print(solution(6, [7, 10]))
